63eb9362ed7f28edbc288ef5fe1c89718e779d6b
eniiku <enikuauth@gmail.com>
2022-09-07 19:10:26 +0100

    setup theme with base configurations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import "typeface-dm-sans";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
