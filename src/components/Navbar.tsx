6066df12b9303571b3560c88ccb1ec925d326ecd
eniiku <enikuauth@gmail.com>
2022-11-12 15:35:51 +0100

    chore(Navbar): rewrite navbar for mobile layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
+import logoIcon from '../assets/Logo.svg';
+
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -9,31 +11,34 @@ export const Navbar = () => {
 
   return (
     <header
-      className="p-8 flex justify-between items-center
-      sticky z-50 "
+      className='p-8 flex justify-between items-center
+      sticky z-50 '
     >
-      <h1 className="font-bold text-2xl">
-        Twin<span>Feed</span>
-      </h1>
+      <div>
+        <img src={logoIcon} alt='logo for TwinsFeed' />
+        <h1 className='font-bold text-2xl'>
+          Twin<span>Feed</span>
+        </h1>
+      </div>
       <AiOutlineMenu size={26} onClick={handleMenuOpen} />
 
       {isMenuOpen && (
         <nav
-          className="fixed h-screen w-4/6 backdrop-blur-xl top-0
-          right-0 table p-8 z-20"
+          className='fixed h-screen w-4/6 backdrop-blur-xl top-0
+          right-0 table p-8 z-20'
         >
           <AiOutlineClose
             size={28}
-            className="absolute right-8 top-8"
+            className='absolute right-8 top-8'
             onClick={handleMenuClose}
           />
-          <ul className=" table-cell align-middle">
-            {['who we are', 'what we do', 'contact'].map((link) => (
+          <ul className=' table-cell align-middle'>
+            {['about us', 'mission', 'contact'].map((link) => (
               <li
                 key={link}
                 onClick={handleMenuClose}
-                className="mb-8 capitalize text-lg py-2 border-b-2 
-                 border-b-black"
+                className='mb-8 capitalize text-lg py-2 border-b-2 
+                 border-b-black'
               >
                 {link}
               </li>
