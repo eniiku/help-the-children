f788cf6f171bd4f41d5237558f9dfc6e9390e4fb
eniiku <enikuauth@gmail.com>
2022-11-14 14:03:58 +0100

    style(About): add basic css declarations to about page
    close: #7

--- src/container/About.tsx
+++ src/container/About.tsx
@@ -1,16 +1,12 @@
-import photoLeft1 from '../assets/jpgs/larm-rmah.jpg';
+import photoLeft1 from '../assets/jpgs/gabriel-rodrigues.jpg';
 import photoLeft2 from '../assets/jpgs/pexels-rodnae-productions.jpg';
 
 const About = () => {
   return (
-    <section>
-      <div>
-        <img src={photoLeft1} alt='something' />
-        <img src={photoLeft2} alt='something' />
-      </div>
-      <div>
-        <h1>who we are</h1>
-        <p>
+    <section className='container grid grid-cols-2'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='playfair-sub'>who we are</h1>
+        <p className='text-sm leading-[1.8] w-4/5 text-left'>
           We are a non-profit organizaiton dedicated to helping the children
           <br />
           We do whatever it takes to make sure children don’t just survive but
@@ -18,6 +14,18 @@ const About = () => {
           mark on their world and help build a better future. We stand the help
           fellow human beings, we are Help The Children.
         </p>
+      </div>
+      <div className='relative'>
+        <img
+          src={photoLeft1}
+          alt='picture of children smiling'
+          className='h-3/5 absolute top-[10%] left-[10%] rounded-lg'
+        />
+        <img
+          src={photoLeft2}
+          alt='picture of volunteer picking goods for donation'
+          className='w-3/5 absolute z-30 bottom-[15%] left-[35%] rounded-lg'
+        />
       </div>
     </section>
   );
