30398c6de0b028864ad522c9f9358248b34ea0b0
eniiku <enikuauth@gmail.com>
2022-11-14 12:27:57 +0100

    style(Donate): add basic css declarations for style and interactivity
    close: #5;

--- src/container/Donate.tsx
+++ src/container/Donate.tsx
@@ -2,30 +2,42 @@ import { AiOutlineArrowRight } from 'react-icons/ai';
 
 export const Donate = () => {
   return (
-    <section>
-      <div>
-        <h1>ready to help?</h1>
+    <section className='container grid place-content-center'>
+      <div className='flex flex-col items-center'>
+        <h1 className='playfair-sub'>ready to help?</h1>
 
-        <p>
+        <p className='w-3/5 text-center leading-[1.7]'>
           You have two hands. One to help yourself, the second the help others.
           Share your kindness through donations here
         </p>
       </div>
 
-      <div>
+      <div className='grid mt-16 grid-cols-4 gap-4'>
         {['$5', '$15', '$45', '$125'].map((amount) => (
-          <button key={amount}>{amount}</button>
+          <button key={amount} className='donate__btn'>
+            {amount}
+          </button>
         ))}
 
-        <button>
-          <form>
+        <button className='donate__btn donate__btn--search'>
+          <form className='w-full flex items-center gap-4'>
             <label htmlFor='custom'>$</label>
 
-            <input type='number' name='custom' placeholder='Custom Amount' />
+            <input
+              type='search'
+              name='custom'
+              placeholder='Custom Amount'
+              className='flex-1 bg-transparent border-l pl-4 placeholder-slate-300
+                outline-none'
+            />
           </form>
         </button>
 
-        <button type='submit'>
+        <button
+          type='submit'
+          className='donate__btn donate__btn--green donate__btn--submit flex items-center 
+            justify-center gap-2'
+        >
           continue
           <AiOutlineArrowRight size={20} />
         </button>
